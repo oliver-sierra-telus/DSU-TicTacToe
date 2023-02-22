50d646a6aaab9d92a2b7694547a4cef8e0695a02
oliver-sierra-telus <oliver.sierra@telus.com>
2023-02-22 16:17:46 -0600

    tictactoc view

---
+++ src/main/java/com/dsu/tictactoe/view/TicTacToeView.java
@@ -0,0 +1,18 @@
+package com.dsu.tictactoe.view;
+
+public class TicTacToeView {
+
+    public void selectPlayers(){
+        
+    }
+
+    public void assignRole(){
+
+    }
+
+    public void play(){
+
+    }
+
+    
+}
